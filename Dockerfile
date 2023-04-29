9180499885d112b1e63b4656a61ac0959d345b24
Alexander Eklund <alexeklund460@gmail.com>
2023-04-29 12:11:30 +0200

    Change to gradle and reset

--- Dockerfile
+++
@@ -1,6 +0,0 @@
-FROM openjdk:17-jdk-alpine
-LABEL authors="Alexander Ekund"
-
-COPY target/EAM-0.0.1-SNAPSHOT.jar app-1.0.0.jar
-
-ENTRYPOINT [ "java", "-jar", "app-1.0.0.jar" ]
--- src/main/kotlin/se/arctic/eam/repository/RoleRepository.kt
+++
@@ -1,9 +0,0 @@
-package se.arctic.eam.repository
-
-import org.springframework.data.jpa.repository.JpaRepository
-import org.springframework.stereotype.Repository
-import se.arctic.eam.model.Role
-import se.arctic.eam.model.User
-
-@Repository
-interface RoleRepository : JpaRepository<Role?, Long?>
