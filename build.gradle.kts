8d7dcdcbd395e17e6c6adabb4ca402e3f5465e46
Alexander Eklund <alexeklund460@gmail.com>
2023-04-29 19:27:44 +0200

    Add liquidbase for database migrations

--- build.gradle.kts
+++ build.gradle.kts
@@ -18,9 +18,16 @@ repositories {
 }
 
 dependencies {
+	implementation("org.springframework.boot:spring-boot-starter-log4j2")
+	modules {
+		module("org.springframework.boot:spring-boot-starter-logging") {
+			replacedBy("org.springframework.boot:spring-boot-starter-log4j2", "Use Log4j2 instead of Logback")
+		}
+	}
 	implementation("org.springframework.boot:spring-boot-starter-data-jpa")
 	implementation("org.springframework.boot:spring-boot-starter-security")
 	implementation("org.springframework.boot:spring-boot-starter-web")
+	implementation("org.liquibase:liquibase-core")
 	implementation("com.fasterxml.jackson.module:jackson-module-kotlin")
 	implementation("org.jetbrains.kotlin:kotlin-reflect")
 	developmentOnly("org.springframework.boot:spring-boot-devtools")
---
+++ src/main/resources/db/changelog/changes/v0001.sql
@@ -0,0 +1,5 @@
+create table "user" (
+    id varchar(255) not null,
+    username varchar(50) not null,
+    primary key (id)
+);
